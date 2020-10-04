fd99f5736fada39e358e7ee340ca35f311f9f3a7
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2020-10-04 19:34:28 +1100

    Start trying to add build system

--- compile.php
+++ compile.php
@@ -1,4 +1,8 @@
 <?php
+$buildDir = __DIR__ . "/build/";
+if (!is_dir($buildDir)) {
+    mkdir($buildDir);
+}
 $compile = TRUE;
 $compile_pages = array(
     "Home" => array("filename" => "home", "compile" => "index"),
@@ -7,7 +11,7 @@ $compile_pages = array(
 foreach ($compile_pages as $page_name => $filenames) {
     ob_start();
     include($filenames["filename"].".php");
-    $outfile = fopen($filenames["compile"].".html", "w") or die("Compile failed: access denied.");
+    $outfile = fopen($buildDir . $filenames["compile"].".html", "w") or die("Compile failed: access denied.");
     fwrite($outfile, ob_get_contents());
     fclose($outfile);
     ob_end_clean();
